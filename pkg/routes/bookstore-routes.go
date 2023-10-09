5428bdfc68f671b34e822d77f0f8f51d2f5e0ea8
to4to <justjustifying@outlook.com>
2023-10-09 20:39:28 +0530

    Routes Created

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,1 +1,17 @@
- 
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/to4to/Go-Bookstore-Management/pkg/controllers"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookBId).Methods("GET")
+
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("book/{bookId}", controllers.DeleteBook).Methods("PUT")
+
+}
