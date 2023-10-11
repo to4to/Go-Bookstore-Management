5f984a72014f820a72a086ad8434a39f69ba9b63
to4to <justjustifying@outlook.com>
2023-10-11 14:32:12 +0530

    routes in main started

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,8 +1,22 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/to4to/Go-Bookstore-Management/pkg/routes"
+)
+
 
 func main(){
 
 
-	
+
+
+	r:=mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+http.Handle("/", r)
+log.Fatal(http.ListenAndServe("localhost:8080",r))
+
 }
