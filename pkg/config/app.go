80ee1933ef688bbc17b496017e0b3cf4cfddfc85
to4to <justjustifying@outlook.com>
2024-10-20 18:54:24 +0530

    Refactor app.go and add database connection using gorm

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,10 +1,15 @@
 package config
 
 import (
+	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-
-var(
-	
+var (
+	db *gorm.DB
 )
+
+
+func Connect(){
+	d,err:=gorm.Open("mysql")
+}
