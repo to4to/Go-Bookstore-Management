73148a4af0fa6f6ab8650b12cf333390a1cbbe32
to4to <justjustifying@outlook.com>
2024-12-08 01:22:34 +0530

    Add CRUD functions for book management and update .gitignore

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -2,7 +2,12 @@ package controllers
 
 import "net/http"
 
+func CreateBook(w http.ResponseWriter, r *http.Request) {}
 
-func CreateBook(w http.ResponseWriter,r *http.Request ){}
+func GetBook(w http.ResponseWriter, r *http.Request) {}
 
-func GetBook(w http.ResponseWriter,r *http.Request ){}
+func GetBookById(w http.ResponseWriter, r *http.Request) {}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {}
