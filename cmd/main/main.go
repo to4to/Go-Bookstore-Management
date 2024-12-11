52aef7f6a784e335fc32e36b668111df27b2b688
to4to <justjustifying@outlook.com>
2024-12-12 02:49:01 +0530

    Implement main function to set up HTTP server and register routes

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/to4to/Go-Bookstore-Management/pkg/routes"
+)
+
+func main() {
+
+	r := mux.NewRouter()
+
+	routes.RegisterRoutes(r)
+	http.Handle("/", r)
+
+	log.Fatal(http.ListenAndServe("localhost:9000", r))
+
+}
