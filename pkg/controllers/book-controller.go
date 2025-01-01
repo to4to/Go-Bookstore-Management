841f1479348ecc866b066abf834858244ee449e6
to4to <justjustifying@outlook.com>
2025-01-01 23:28:31 +0530

    Implement CreateBook function to retrieve and return all books in JSON format; add VSCode settings file

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,11 +1,25 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/to4to/Go-Bookstore-Management/pkg/models"
+)
 
 func CreateBook(w http.ResponseWriter, r *http.Request) {
-	
+
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
+// GetBook handles the HTTP request to retrieve a book by its ID.
+// It extracts the book ID from the request, fetches the corresponding book
+// from the database, and writes the book details to the response.
+// If the book is not found, it returns an appropriate error message.
 func GetBook(w http.ResponseWriter, r *http.Request) {}
 
 func GetBookById(w http.ResponseWriter, r *http.Request) {}
