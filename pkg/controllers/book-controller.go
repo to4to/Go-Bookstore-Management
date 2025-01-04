d58ad4dfb06b9224eb2cc59735bbb808097895c3
to4to <justjustifying@outlook.com>
2025-01-04 20:56:08 +0530

    Implement GetBookById, UpdateBook, and DeleteBook functions in book-controller

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -29,10 +29,83 @@ func GetBook(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func GetBookById(w http.ResponseWriter, r *http.Request) {
-	getbook:=
+
+	func GetBookById(w http.ResponseWriter, r *http.Request) {
+    // Get book ID from URL parameters
+    vars := mux.Vars(r)
+    bookId := vars["bookId"]
+    ID, err := strconv.ParseInt(bookId, 0, 0)
+    if err != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+
+    // Get book details from model
+    book, _ := models.GetBookById(ID)
+
+    // Return JSON response
+    res, _ := json.Marshal(book)
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK) 
+    w.Write(res)
 }
 
-func UpdateBook(w http.ResponseWriter, r *http.Request) {}
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+    // Get book ID from URL parameters
+    vars := mux.Vars(r)
+    bookId := vars["bookId"]
+    ID, err := strconv.ParseInt(bookId, 0, 0)
+    if err != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
 
-func DeleteBook(w http.ResponseWriter, r *http.Request) {}
+    // Parse request body
+    var updateBook models.Book
+    decoder := json.NewDecoder(r.Body)
+    if err := decoder.Decode(&updateBook); err != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+    defer r.Body.Close()
+
+    // Update book in database
+    bookDetails, db := models.GetBookById(ID)
+    if updateBook.Name != "" {
+        bookDetails.Name = updateBook.Name
+    }
+    if updateBook.Author != "" {
+        bookDetails.Author = updateBook.Author
+    }
+    if updateBook.Publication != "" {
+        bookDetails.Publication = updateBook.Publication
+    }
+    db.Save(&bookDetails)
+
+    // Return updated book
+    res, _ := json.Marshal(bookDetails)
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+    // Get book ID from URL parameters
+    vars := mux.Vars(r)
+    bookId := vars["bookId"]
+    ID, err := strconv.ParseInt(bookId, 0, 0)
+    if err != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+
+    // Delete book from database
+    book := models.DeleteBook(ID)
+    
+    // Return deleted book details
+    res, _ := json.Marshal(book)
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(res)
+}	
+
