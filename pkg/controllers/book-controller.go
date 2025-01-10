8a01ba4cc0d8b649601032d4107ec81db8501cc6
to4to <justjustifying@outlook.com>
2025-01-10 18:40:06 +0530

    Implement GetBook function to retrieve all books and return them in JSON format

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,11 @@
 package controllers
+
+
+
+func GetBook(w http.ResponseWriter, r *http.Request){
+	newBooks:=models.GetAllBooks()
+	res, _ :=json.Marshal(newBooks)
+	w.Header().Set("Content-Type","pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
