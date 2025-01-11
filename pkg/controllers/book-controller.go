8bd395a0544d0f4b076d47172e97f7441b227a23
to4to <justjustifying@outlook.com>
2025-01-11 23:37:36 +0530

    Remove unused GetBook function from book-controller

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,11 +1,3 @@
 package controllers
 
 
-
-func GetBook(w http.ResponseWriter, r *http.Request){
-	newBooks:=models.GetAllBooks()
-	res, _ :=json.Marshal(newBooks)
-	w.Header().Set("Content-Type","pkglication/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
-}
