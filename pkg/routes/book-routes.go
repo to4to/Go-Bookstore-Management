51cb2797d012b68e18ae5c29b5202ecffb76cbad
to4to <justjustifying@outlook.com>
2025-01-13 23:40:00 +0530

    Remove unused PUT route for updating a book in book-routes

--- pkg/routes/book-routes.go
+++ pkg/routes/book-routes.go
@@ -17,7 +17,4 @@ var RegisterRoutes = func(routes *mux.Router) {
 	routes.HandleFunc("/book", controllers.CreateBook).Methods("POST")
 	routes.HandleFunc("/book", controllers.GetBook).Methods("GET")
 	routes.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
-	routes.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
-
-
 }
