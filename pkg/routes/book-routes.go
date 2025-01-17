bcd75b35f3b76f5a55124f8076da8875ac676a90
to4to <justjustifying@outlook.com>
2025-01-17 23:43:25 +0530

    Remove unused GET route for retrieving a book by ID in book-routes

--- pkg/routes/book-routes.go
+++ pkg/routes/book-routes.go
@@ -16,6 +16,6 @@ var RegisterRoutes = func(routes *mux.Router) {
 
 	routes.HandleFunc("/book", controllers.CreateBook).Methods("POST")
 	routes.HandleFunc("/book", controllers.GetBook).Methods("GET")
-	routes.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	
 	
 }
